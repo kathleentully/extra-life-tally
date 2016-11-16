8fca73a6f565d799451a6ecb3302815bed1c9f44
Kathleen Tully <kathleen@Rajah.local>
2016-11-16 13:02:47 -0500

    basic calculation

---
+++ main.py
@@ -0,0 +1,33 @@
+import json, urllib, csv
+
+teamRosterUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.teamParticipants&teamID={teamId}&format=json'
+participantUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID={participantId}&format=json'
+teamUrl = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.team&teamID={teamId}&format=json'
+reference = json.loads(open('reference.json').read())
+
+def getTeamRoster():
+    teamRosterWithMoneyRaised = {}
+    sourceRoster = json.loads(urllib.urlopen(teamRosterUrl.format(teamId = reference['team number'])).read())
+    for participant in sourceRoster:
+        teamRosterWithMoneyRaised[participant['participantID']] = json.loads(urllib.urlopen(participantUrl.format(participantId = participant['participantID'])).read())
+    return teamRosterWithMoneyRaised
+
+def getSubTeamTotals(teamRoster):
+    subteams = {}
+    for subteamName, subteamParticipants in reference['teams'].iteritems():
+        subteams[subteamName] = {'raised': 0, 'goal': 0}
+        for participant in subteamParticipants:
+            subteams[subteamName]['raised'] += teamRoster[participant['participantID']]['totalRaisedAmount']
+            subteams[subteamName]['goal'] += teamRoster[participant['participantID']]['fundraisingGoal']
+    return subteams
+
+def sortResults(subteamTotals):
+    return sorted(subteamTotals.items(), key = lambda item: item[1]['raised'], reverse = True)
+
+def printResults(subteamTotals):
+    for team in sortResults(subteamTotals):
+        print('{0:14} ${1} / ${2}'.format(team[0], team[1]['raised'], team[1]['goal']))
+
+if __name__ == "__main__":
+    printResults(getSubTeamTotals(getTeamRoster()))
+    
